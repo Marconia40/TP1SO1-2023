85a237b85bda01bfd33e6d65153918ff52c756b4
Marconia40 <mendrek@unc.edu.ar>
2023-08-24 23:59:44 -0300

    Obtencion de los datos de Memoria RAM del archivo e impresos en valores de MB

--- src/lab1.c
+++ src/lab1.c
@@ -5,8 +5,12 @@
 int main()
 {
     FILE    *archivo;
-
-
+    char    info[20];
+    //char    Hola[5] = "hola";
+    int     valor[3];
+    char    MemT[15] = "MemTotal:";
+    char    MemF[15] = "MemFree:";
+    char    MemA[15] = "MemAvailable:";
 
     archivo = fopen("/proc/meminfo", "r");
     if (archivo == NULL){
@@ -16,7 +20,23 @@ int main()
         printf("%s","El archivo /proc/meminfo se abrio correctamente.\n");
     }
 
+    while(fscanf(archivo,"%s", info) == 1){
+        if(strcmp(info , MemT) == 0){
+            fscanf(archivo, "%d", &valor[0]);
+        }
+        else if(strcmp(info , MemF) == 0){
+            fscanf(archivo, "%d", &valor[1]);
+        }
+        else if(strcmp(info, MemA) == 0){
+            fscanf(archivo, "%d", &valor[2]);
+        }
+    }
 
+    
+    printf("%s %d MB. \n", MemT,valor[0]/1000);
+    printf("%s %d MB. \n", MemF,valor[1]/1000);
+    printf("%s %d MB. \n", MemA,valor[2]/1000);
+    
     fclose(archivo);
     return 0;
 }
